d2a4c3a3e8fc4edc0277519da78bbefea2620f51
shawnbarron16 <shawnbarron16@gmail.com>
2023-05-23 22:29:49 -0500

    Created basic file structue

--- src/App.svelte
+++ src/App.svelte
@@ -3,9 +3,14 @@
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
+	<h1>“When you launch in a rocket, you’re not really flying that rocket.
+		You’re just sort of hanging on.” - Michael P. Anderson</h1>
 </main>
 
 <style>
-	
+	main{
+		max-width: 1000px;
+		margin: 40px auto;
+		text-align: center;
+	}
 </style>
