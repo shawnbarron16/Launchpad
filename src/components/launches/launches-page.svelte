f46b7c6925eba8f085448de5cdc6d1d6b540816e
shawnbarron16 <shawnbarron16@gmail.com>
2023-05-26 09:02:21 -0500

    Launchpad can now get data from firestore

--- src/components/launches/launches-page.svelte
+++ src/components/launches/launches-page.svelte
@@ -1,9 +1,28 @@
 <script>
+    import { db } from "../../firebase";
 
+    let launches = []
+    //Get every document from the db, looping over each one and run a function to render some html using the data from that document
+    db.collection('launches').get().then((snapshot) => {
+        snapshot.docs.forEach(document => {
+            launches.push(document.data())
+            launches = launches
+        })
+    })
+
+    console.log("Launches array:", launches)
 </script>
 
 <main>
     <h1>Launches</h1>
+    {#each launches as launch (launch.id)}
+        <div>
+            <p>Agency: {launch.agency}</p>
+            <p>Model: {launch.model}</p>
+            <p>Past launches: {launch.past_launches}</p>
+            <p>Is milestone: {launch.is_milestone}</p>
+        </div>
+    {/each}
 </main>
 
 <style>
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,6 @@
 // Import the functions you need from the SDKs you need
 import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,8 +17,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
 
 export const db = firebase.firestore();
-db.settings({ timestampsInSnapshots: true})
+db.settings({ timestampsInSnapshots: true}) 
