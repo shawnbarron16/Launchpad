0c97eac17cb49ef4f240a9e32eac1ebf2918b8b3
shawnbarron16 <shawnbarron16@gmail.com>
2023-06-09 17:14:56 -0500

    Turned the cloud function from an http request function into a scheduled function

--- functions/index.js
+++ functions/index.js
@@ -4,30 +4,32 @@ const axios = require("axios");
 
 admin.initializeApp();
 
-exports.fetchUpcomingLaunches = functions.https.onRequest(async (req, res) => {
-  try {
-    const apiUrl = "https://lldev.thespacedevs.com/2.2.0/launch/upcoming";
-    const response = await axios.get(apiUrl);
-    const data = response.data.results;
+exports.fetchLaunches = functions
+    .pubsub.schedule("0 * * * *")
+    .onRun(async (req, res) => {
+      try {
+        const apiUrl = "https://ll.thespacedevs.com/2.2.0/launch/upcoming";
+        const response = await axios.get(apiUrl);
+        const data = response.data.results;
 
-    const firestore = admin.firestore();
-    const launchesCollection = firestore.collection("launches");
+        const firestore = admin.firestore();
+        const launchesCollection = firestore.collection("launches");
 
-    // Clear existing documents in the collection before saving new data
-    await launchesCollection.get().then((snapshot) => {
-      snapshot.forEach((doc) => {
-        doc.ref.delete();
-      });
+        // Clear existing documents in the collection before saving new data
+        await launchesCollection.get().then((snapshot) => {
+          snapshot.forEach((doc) => {
+            doc.ref.delete();
+          });
+        });
+
+        // Save each launch document to the collection
+        data.forEach((launch) => {
+          launchesCollection.add(launch);
+        });
+
+        res.status(200).send("Data fetched and saved successfully!");
+      } catch (error) {
+        console.error("Error fetching or saving data:", error);
+        res.status(500).send("Error fetching or saving data");
+      }
     });
-
-    // Save each launch document to the collection
-    data.forEach((launch) => {
-      launchesCollection.add(launch);
-    });
-
-    res.status(200).send("Data fetched and saved successfully!");
-  } catch (error) {
-    console.error("Error fetching or saving data:", error);
-    res.status(500).send("Error fetching or saving data");
-  }
-});
