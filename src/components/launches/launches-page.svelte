4624e4894b7ad0c09f7c42303aa2d0dbe92c6297
shawnbarron16 <shawnbarron16@gmail.com>
2023-06-09 18:45:10 -0500

    Moved launch picture on launches feed

--- src/components/launches/launches-page.svelte
+++ src/components/launches/launches-page.svelte
@@ -12,28 +12,37 @@
     <!--Access the Article Store and render the results-->
     <h1>Launches</h1>
     <div class="launches">
-        {#each $LaunchStore as launch (launch.id)}
-            <div class="launch-display">
-                <p class="model">
-                    Model: {launch.rocket.configuration.full_name}
-                </p>
-                <p>
-                    Agency: {launch.launch_service_provider.name}
-                    <img src={launch.image} alt="rocket" class="rocket-img" />
-                </p>
-                <p>
-                    Number of launches for {launch.launch_service_provider.name}
-                    this year: {launch.agency_launch_attempt_count_year}
-                </p>
-                <p>
-                    Mission type: {launch.mission.type}
-                </p>
-                    Launch Window: <br>
-                    Start: {formatTime(launch.window_start)} <br>
-                    End: {formatTime(launch.window_end)}
-            </div>
-        {/each}
-    </div>
+		{#each $LaunchStore as launch, i}
+				<div class="launch-display">
+					<div class="launch-content">
+						<p class="model">
+							Model: {launch.rocket.configuration.full_name}
+						</p>
+						<img
+									src={launch.image}
+									alt="rocket"
+									class="rocket-img"
+								/>
+						<div class="launch-details">
+							<p>
+								Agency: {launch.launch_service_provider.name}
+							</p>
+							<p>
+								Number of launches for {launch.launch_service_provider.name} this year: {launch.agency_launch_attempt_count_year}
+							</p>
+							<p>
+								Mission type: {launch.mission.type}
+							</p>
+							<p>
+								Launch Window: <br />
+								Start: {formatTime(launch.window_start)} <br />
+								End: {formatTime(launch.window_end)}
+							</p>
+						</div>
+					</div>
+				</div>
+		{/each}
+	</div>
 </main>
 
 <style>
@@ -52,23 +61,39 @@
     }
 
     .launches {
-        display: flex;
-        flex-direction: column;
-    }
+		display: flex;
+		flex-direction: column;
+	}
 
-    .launch-display {
-        text-align: left;
-        border-style: double;
-        margin-bottom: 25px;
-    }
+	.launch-display {
+		text-align: left;
+		border-style: double;
+		margin-bottom: 25px;
+	}
 
-    .model {
-        text-align: center;
-        font-weight: 1000;
-        font-size: larger;
-    }
+	.model {
+		text-align: center;
+		font-weight: 1000;
+		font-size: larger;
+	}
 
-    .rocket-img {
-        float: right;
-    }
+	.rocket-img {
+		float: right;
+		margin-top: 5px;
+		width: 300px;
+		height: 200px;
+	}
+
+	/* Media query for mobile devices */
+	@media (max-width: 768px) {
+		.launch-content {
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+		}
+		
+		.rocket-img {
+			float: none;
+		}
+	}
 </style>
